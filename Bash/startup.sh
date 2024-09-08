85daf06cd0cbcfdb6c87080da114207d6f3e3fb0
CyanideData2 <danielhurtadolabandeira@gmail.com>
2024-09-08 02:23:15 +0200

    achieved boot and keyboard manager

--- Bash/startup.sh
+++ Bash/startup.sh
@@ -1,16 +1,18 @@
 #! /usr/bin/bash
 
-#keyboard
-PLUGGEDKEYCHRON=/dev/input/by-id/usb-Keychron_Keychron_K6-event-kbd
-SPICE=/home/daniel/V/Workspace/my_config/kmonad/keychronColemak.kbd
-PLAIN=/home/daniel/V/Workspace/my_config/kmonad/colemak.kbd
-FOREIGN=/home/daniel/V/Workspace/my_config/kmonad/nordic.kbd
-/home/daniel/.local/bin/kmonad $PLAIN &
-if [ -e "$PLUGGEDKEYCHRON" ]
-then
-    /home/daniel/.local/bin/kmonad $SPICE
-fi
-while inotifywait -e create /dev/input/; do
-        /home/daniel/.local/bin/kmonad $SPICE
-        /home/daniel/.local/bin/kmonad $FOREIGN
-done
+# KMONAD
+# PLUGGEDKEYCHRON=/dev/input/by-id/usb-Keychron_Keychron_K6-event-kbd
+# SPICE=/home/daniel/V/Workspace/my_config/kmonad/keychronColemak.kbd
+# PLAIN=/home/daniel/V/Workspace/my_config/kmonad/colemak.kbd
+# FOREIGN=/home/daniel/V/Workspace/my_config/kmonad/nordic.kbd
+# /home/daniel/.local/bin/kmonad $PLAIN &
+# if [ -e "$PLUGGEDKEYCHRON" ]
+# then
+#     /home/daniel/.local/bin/kmonad $SPICE
+# fi
+# while inotifywait -e create /dev/input/; do
+#         /home/daniel/.local/bin/kmonad $SPICE
+#         /home/daniel/.local/bin/kmonad $FOREIGN
+# done
+#
+/home/daniel/.cargo/bin/kanata -c /home/daniel/V/Workspace/my_config/kanata/main.kbd
